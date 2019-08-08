069dde71511cec4cf8426759682d1fcdc62be0d0
Jorge Lopez <jorge.lopez@riseup.net>
2019-08-08 17:10:54 +0200

    Comprobado usuario y no solo token en el middleware de autorizacion

--- middleware/authorizationMiddleware.js
+++ middleware/authorizationMiddleware.js
@@ -2,26 +2,43 @@ const jwt = require("jsonwebtoken");
 const settings = require("../settings");
 const UserDao = require("../daos/userDao");
 
-let authorization = function(req, res, next){
-    let token = req.headers['authorization']
-    if(!token){
-        res.status(401).send({
-          error: "Es necesario el token de autenticación"
-        })
-        return
-    }
- 
-    token = token.replace('Bearer ', '')
- 
-    jwt.verify(token, settings.JWT_PASSWORD, function(err, test) {
-      if (err) {
-        res.status(401).send({
-          error: 'Token inválido'
-        })
-      } else {
-          next();
-      }
+let authorization = function (req, res, next) {
+  let token = req.headers['authorization']
+  if (!token) {
+    res.status(401).send({
+      error: "Es necesario el token de autenticación"
     })
+    return
+  }
+
+  token = token.replace('Bearer ', '')
+
+  jwt.verify(token, settings.JWT_PASSWORD, function (err, decoded) {
+    if (err) {
+      res.status(401).send({
+        error: 'Token inválido'
+      })
+    } else {
+      let userDao = new UserDao();
+      userDao.findById(decoded.id)
+        .then((user) => {
+          if (!user.isAdmin) {
+            res.status(401).send({
+              error: "Usuario no admin"
+            });
+
+            return;
+          }
+
+          next();
+        })
+        .catch(() => {
+          res.status(401).send({
+            error: "Usuario no admin"
+          });
+        })
+    }
+  })
 }
 
 let isAdmin = function(req, res, next){
@@ -32,16 +49,21 @@ let isAdmin = function(req, res, next){
   let userDao = new UserDao();
   userDao.findById(decoded.id)
     .then((user) => {
-      if(!user.isAdmin){
+      if (!user.isAdmin) {
         res.status(401).send({
           error: "Usuario no admin"
         });
-        
+
         return;
       }
 
       next();
-    });
+    })
+    .catch(() => {
+      res.status(401).send({
+        error: "Usuario no admin"
+      });
+    })
 }
 
 module.exports = {
